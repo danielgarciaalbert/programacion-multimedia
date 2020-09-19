538b0b52cc9442de55ddf8462a4e75e7db5ee97f
Daniel García Albert <garciaalbertdaniel@gmail.com>
2020-09-19 18:45:24 +0200

    New version of Marcianos
    I deleted "temp" and "library" folders because they were so heavy. Now, the enemies have animations, and they can collide with the space ship

---
+++ Marcianos/Assets/Scripts/Enemigo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    float velocidadX = 2;
+    float velocidadY = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(velocidadX * Time.deltaTime, 
+            0, 0);
+
+        if (transform.position.x < -4 || transform.position.x > 4)
+        {
+            transform.position = new Vector3(
+                transform.position.x, transform.position.y - 0.5f, 0f);
+            velocidadX = -velocidadX;
+        }
+
+        /* if (transform.position.y < -2.5 || transform.position.y > 2.5)
+            velocidadY = -velocidadY; */
+    }
+}
---
+++ Marcianos/Assets/Scripts/Nave.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nave : MonoBehaviour
+{
+    [SerializeField] float velocidad = 2;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(horizontal * velocidad * Time.deltaTime, 0, 0);
+
+        //Estableciendo límites laterales para la nave
+        if (transform.position.x < -4.25)
+        {
+            transform.position = new Vector3(   
+                -4.25f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > 4.25 )
+        {
+            transform.position = new Vector3(
+                4.25f, transform.position.y, transform.position.z);
+        }
+    }   
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Golpeado");
+    }
+}
