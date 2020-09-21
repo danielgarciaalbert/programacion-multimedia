ffae1a1f698c2d46ffcfffea795c83d8fba9b5e5
Daniel García Albert <garciaalbertdaniel@gmail.com>
2020-09-21 21:06:46 +0200

    More updates of Marcianos

--- Marcianos/Assets/Scripts/DisparoEnemigo.cs
+++ Marcianos/Assets/Scripts/DisparoEnemigo.cs
@@ -13,7 +13,7 @@ public class DisparoEnemigo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
